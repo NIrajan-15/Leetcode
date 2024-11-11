2b8dbeec43b321ebdad73c621e3120ba856a2621
Nirajan Sangraula <57874425+NIrajan-15@users.noreply.github.com>
2024-11-11 13:51:25 -0600

    Time: 87 ms (5.26%), Space: 44.4 MB (22.15%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    public String countAndSay(int n) {
+        String encoded = "1";
+        for(int i=1;i<n;i++)
+        {
+            encoded = encode(encoded);
+
+        }
+        return encoded;
+    }
+
+    private String encode(String st)
+    {
+        StringBuilder result = new StringBuilder();
+        
+        char current = st.charAt(0);
+        int count = 1;
+       for(int i=1;i<st.length();i++)
+       {
+            if(st.charAt(i)!=current)
+            {
+                System.out.println(count+" "+current);
+                result.append(count);
+                result.append(current);
+                current = st.charAt(i);
+                count=1;
+            }
+            else
+            {
+                count++;
+            }
+       }
+       System.out.println(count+" "+current);
+       result.append(count);
+       result.append(current);
+       return result.toString();
+    }
+}
