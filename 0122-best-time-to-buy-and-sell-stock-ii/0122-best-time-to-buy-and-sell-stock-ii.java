0d8b54bb05e13560dacadf5c326aa1a5f11d7ed7
Nirajan Sangraula <57874425+NIrajan-15@users.noreply.github.com>
2024-11-13 11:44:32 -0600

    Time: 1 ms (91.82%), Space: 46 MB (18.44%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,9 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        
         int profit = 0;
         int buy = prices[0];
-        int minV = Integer.MIN_VALUE;
-        int oProfit = 0;
-        int sell = 0;
-        boolean buyAgain = false;
+
         for(int i=1;i<prices.length;i++)
         {
             if(prices[i]>buy)
@@ -17,11 +15,8 @@ class Solution {
             {
                 buy = prices[i];
             }
-
-            minV = Math.min(minV, prices[i]);
-            oProfit += Math.max(oProfit, prices[i]-minV+oProfit);
         }
-        return Math.max(profit, oProfit);
+        return profit;
     }
 }
 
