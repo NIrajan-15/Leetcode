b4d74bc1e26a4e5d548ff2105f65f3fb570597a5
Nirajan Sangraula <57874425+NIrajan-15@users.noreply.github.com>
2024-11-13 12:31:12 -0600

    Time: 366 ms (5.44%), Space: 60.7 MB (53.98%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] ans = new int[temperatures.length];
+            for(int i=0;i<ans.length-1;i++)
+            {
+                if(i>0 && temperatures[i]==temperatures[i-1])
+                {
+                    if(ans[i-1]!=0){
+                        ans[i] = ans[i-1]-1;
+                    }else
+                    {
+                        ans[i] = 0;
+                    }
+                    continue;
+                }
+                int j=i+1;
+                while(j<ans.length&&temperatures[j]<=temperatures[i])
+                {
+                    j++;
+                }
+                if(j>=ans.length) ans[i]=0;
+                else ans[i] = j-i;
+            }
+            return ans;
+        }
+}
